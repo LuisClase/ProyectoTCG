0a91e734d5dafd106f5243ef03d3b6bb82a60fee
Luis Cerqueira <luis.cerqueira.estevez@gmail.com>
2015-03-11 14:01:25 +0100

    dia 13 con gitv8

--- app/src/main/java/com/example/luiscerqueira/proyectotcg/ActivityCombinar.java
+++ app/src/main/java/com/example/luiscerqueira/proyectotcg/ActivityCombinar.java
@@ -2,11 +2,15 @@ package com.example.luiscerqueira.proyectotcg;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
@@ -29,38 +33,35 @@ public class ActivityCombinar extends ActionBarActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
     public Bitmap imagenCombinada;
+    Bitmap imagenMarco;
+    Bitmap imagenLinea;
+    Bitmap imagenColumna;
+    Bitmap imagenTemp;
     ImageView imagenCentral;
     ImageView imagenCarta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_combinar);
+        imagenLinea=BitmapFactory.decodeResource(getResources(),R.drawable.imagen3);
+        imagenColumna=BitmapFactory.decodeResource(getResources(),R.drawable.imagen2);
+        imagenTemp=BitmapFactory.decodeResource(getResources(),R.drawable.imagen);
         imagenCarta=(ImageView)findViewById(R.id.imageView);
         imagenCentral=(ImageView)findViewById(R.id.imageView2);
+        imagenMarco=((BitmapDrawable)imagenCarta.getDrawable()).getBitmap();
         Button btnCombinar=(Button)findViewById(R.id.btnCombinar);
         Button btnCaracteristicas=(Button)findViewById(R.id.btnCaracteristicas);
         Button btnImagen=(Button)findViewById(R.id.btnImagen);
 
-        Bitmap imagen1=((BitmapDrawable)imagenCarta.getDrawable()).getBitmap();
-            Bitmap imagen2=((BitmapDrawable)imagenCentral.getDrawable()).getBitmap();
+        Bitmap imagen1=imagenMarco;
+        Bitmap imagen2=((BitmapDrawable)imagenCentral.getDrawable()).getBitmap();
         imagenCombinada=overlay(imagen1,imagen2);
 
-        btnCombinar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap imagen1=((BitmapDrawable)imagenCarta.getDrawable()).getBitmap();
-                Bitmap imagen2=((BitmapDrawable)imagenCentral.getDrawable()).getBitmap();
-                imagenCombinada=overlay(imagen1,imagen2);
-                DialogoCombinar dCombinar=new DialogoCombinar();
-                dCombinar.show(getFragmentManager(),"Combinar");
-            }
-        });
         btnImagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(
                         Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
@@ -94,17 +95,66 @@ public class ActivityCombinar extends ActionBarActivity {
             }
             imagenCentral.setImageBitmap(bImagen);
 
+            Bitmap imagen1=imagenMarco;
+            Bitmap imagen2=((BitmapDrawable)imagenCentral.getDrawable()).getBitmap();
+            imagenCombinada=overlay(imagen1,imagen2);
+            imagenCarta.setImageBitmap(imagenCombinada);
             Log.i("IMAGEN","NULL3?"+(bImagen==null?"SI":"NO")+" "+picturePath);
             Log.i("IMAGEN","NULL2?"+(imagenCentral.getDrawable()==null?"SI":"NO"));
 
         }
     }
 
-    public static Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
+    public Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
+//        bmp2=Bitmap.createScaledBitmap(bmp2,(int)(bmp1.getWidth()/1.2),(int)(bmp1.getHeight()/1.5),true);
+        bmp2 = Bitmap.createScaledBitmap(bmp2, imagenTemp.getWidth(), imagenTemp.getHeight(), true);
         Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
         Canvas canvas = new Canvas(bmOverlay);
+//        canvas.drawBitmap(bmp2, bmp1.getWidth()/7, bmp1.getHeight()/7, null);
+        canvas.drawBitmap(bmp2, imagenColumna.getWidth(), imagenLinea.getHeight(), null);
         canvas.drawBitmap(bmp1, new Matrix(), null);
-        canvas.drawBitmap(bmp2, 0, 0, null);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        int tamañoLetra = 100;
+        paint.setTextSize(tamañoLetra);
+        String texto = "Prueba \nTexto \nCarta";
+//        Rect bounds = new Rect();/
+//
+//        paint.getTextBounds(texto, 0, texto.length(), bounds);
+        float tamañoTexto = paint.measureText(texto);
+        float tempX = bmp1.getWidth() - (bmp1.getWidth() - (float) (bmp1.getWidth() * 0.1));
+        float tempY = bmp1.getHeight() - (bmp1.getHeight() - (float) (bmp1.getHeight() * 0.81));
+
+        Float alto = Math.abs(paint.ascent() + paint.descent());
+        while (tamañoTexto > ((bmp1.getWidth() - imagenColumna.getWidth()) - tempX)) {
+            tamañoLetra--;
+            paint.setTextSize(tamañoLetra);
+            tamañoTexto = paint.measureText(texto);
+            Log.i("LETRA", "TAMAÑO MAXIMO: " + ((bmp1.getWidth() - imagenColumna.getWidth()) - tempX));
+            Log.i("LETRA", "TAMAÑO TEXTO: " + tamañoTexto);
+            Log.i("LETRA", "TAMAÑO: " + tamañoLetra);
+        }
+//        paint.getTextBounds(texto, 0, texto.length(), bounds);
+        alto = Math.abs(paint.ascent()) + Math.abs(paint.descent());
+
+        while(alto>((bmp1.getHeight()-imagenLinea.getHeight())-(tempY))){
+            tamañoLetra--;
+            paint.setTextSize(tamañoLetra);
+            alto = Math.abs(paint.ascent()) + Math.abs(paint.descent());
+
+            Log.i("LETRA2","TAMAÑO MAXIMO: "+((bmp1.getHeight()-imagenLinea.getHeight())-(tempY)));
+            Log.i("LETRA2","TAMAÑO: "+tamañoLetra);
+            Log.i("LETRA2","ALTO: "+alto);
+        }
+
+        Log.i("LETRA3", "FUERA WHILE: " + tamañoLetra);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawRect(tempX,tempY-paint.getTextSize(),bmp1.getWidth()-imagenColumna.getWidth(),bmp1.getHeight()-imagenLinea.getHeight(),paint);
+
+        paint.setColor(Color.BLACK);
+        canvas.drawText(texto,tempX,tempY,paint);
         return bmOverlay;
     }
 
